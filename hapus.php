009d12fd593bd2ac3e5b073e242d3ef172813711
Angga <anggadwiat12@gmail.com>
2025-06-09 09:41:46 +0700

    Update hapus.php

--- hapus.php
+++ hapus.php
@@ -4,18 +4,14 @@ include 'koneksi.php';
 if (isset($_GET['id_portofolio'])) {
     $id_portofolio = $_GET['id_portofolio'];
     
-    // Query untuk menghapus data
     $query = "DELETE FROM tb_portofolio WHERE id_portofolio = '$id_portofolio'";
     
     if (mysqli_query($koneksi, $query)) {
-        // Redirect ke halaman utama setelah berhasil hapus
-        header("Location: index.php?status=sukses");
+        header("Location: index.html?status=sukses");
     } else {
-        // Redirect dengan status gagal jika error
-        header("Location: index.php?status=gagal");
+        header("Location: index.html?status=gagal");
     }
 } else {
-    // Redirect jika tidak ada ID
-    header("Location: index.php");
+    header("Location: index.html");
 }
 ?>
