071d9acbdae86202b2e8cd4c281b06c4dda6e555
Angga <anggadwiat12@gmail.com>
2025-06-09 09:42:59 +0700

    Update tambah.php

--- tambah.php
+++ tambah.php
@@ -6,22 +6,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $nama_kegiatan = mysqli_real_escape_string($koneksi, $_POST['nama_kegiatan']);
     $waktu_kegiatan = mysqli_real_escape_string($koneksi, $_POST['waktu_kegiatan']);
     
-    // Cek apakah NIM sudah ada
     $cek = mysqli_query($koneksi, "SELECT * FROM tb_portofolio WHERE id_portofolio = '$id_portofolio'");
     if (mysqli_num_rows($cek) > 0) {
-        header("Location: index.php?status=gagal&pesan=id_portofolio sudah terdaftar");
+        header("Location: index.html?status=gagal&pesan=id_portofolio sudah terdaftar");
         exit();
     }
     
-    // Query tambah data
     $query = "INSERT INTO tb_portofolio (id_portofolio, nama_kegiatan, waktu_kegiatan) VALUES ('$id_portofolio', '$nama_kegiatan', '$waktu_kegiatan')";
     
     if (mysqli_query($koneksi, $query)) {
-        header("Location: index.php?status=sukses");
+        header("Location: index.html?status=sukses");
     } else {
-        header("Location: index.php?status=gagal");
+        header("Location: index.html?status=gagal");
     }
 } else {
-    header("Location: index.php");
+    header("Location: index.html");
 }
 ?>
